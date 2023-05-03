c8865c68fe25e290ce563c643b58a55a85ed29f7
libeu816 <li.beu816@gmail.com>
2023-05-03 09:41:49 +0900

    Feed

--- HH_SNS/src/main/java/edu/spring/ex06/persistence/FeedDAOImple.java
+++ HH_SNS/src/main/java/edu/spring/ex06/persistence/FeedDAOImple.java
@@ -18,41 +18,50 @@ public class FeedDAOImple implements FeedDAO{
 
 	@Autowired
 	private SqlSession sqlSession;
+	
+	/*
+	 * int insert(String userId, FeedVO vo); // 피드 작성(회원)
+	   List<FeedVO> selectAll(String userId, Date feedDate); // 전체 피드 츌력 = 전체 검색 (최신순)(비회원)
+	   FeedVO select (String userId, int feedId); // 피드 상세 출력 = 아이디 기준 검색(비회원)
+	   int update(String userId, FeedVO vo); // 피드 수정(회원)
+	   int delete(String usserId); // 아이디 기준 피드 삭제(회원)
+	   List<FeedVO> selectAllbyId (String userId); // 개인 피드 출력 = 아이디 기준(회원)
+	 * */
 
 	@Override
-	public int insert(String userId, FeedVO vo) {
-		logger.info("insert() 호출");
+	public int insert(FeedVO vo) {
+		logger.info("★ FeedDAOImple 피드 등록");
 		return sqlSession.insert(NAMESPACE + ".insert", vo);
 	}// end insert
 
 	@Override
-	public List<FeedVO> select(Date feedDate) {
-
-		return null;
-	}// end select 전체 검색
+	public List<FeedVO> selectAll(Date feedDate) {
+		logger.info("★ FeedDAOImple 전체검색 최신순");
+		return sqlSession.selectList(NAMESPACE + ".select_all", feedDate);
+	}// end selectAll 전체 검색
 
 	@Override
 	public FeedVO select(String userId, int feedId) {
-
-		return null;
+		logger.info("★ FeedDAOImple 피드 detail");
+		return sqlSession.selectOne(NAMESPACE + ".select", feedId);
 	}// end select 상세 검색
 
 	@Override
-	public int update(String userId, FeedVO vo) {
-
-		return 0;
+	public int update(String userId, int feedId, FeedVO vo) {
+		logger.info("★ FeedDAOImple 피드 수정");
+		return sqlSession.update(NAMESPACE + ".update", vo);
 	}// end update 수정
 
 	@Override
-	public int delete(String usserId) {
-
-		return 0;
+	public int delete(String usserId, int feedId) {
+		logger.info("★ FeedDAOImple 피드 삭제");
+		return sqlSession.delete(NAMESPACE + ".delete", feedId);
 	}// end delete 삭제
 
 	@Override
-	public List<FeedVO> selectAllById(String userId) {
-
-		return null;
-	}// end selectAllById 개인 피드 전체 출력
+	public List<FeedVO> selectAllbyId(String userId) {
+		logger.info("★ FeedDAOImple 개인 피드");
+		return sqlSession.selectList(NAMESPACE +".select_all_by_id", userId);
+	}// end selectAllbyId 개인 피드 전체 출력
 
 }
--- HH_SNS/src/test/java/edu/spring/ex06/FeedDAOTest.java
+++ HH_SNS/src/test/java/edu/spring/ex06/FeedDAOTest.java
@@ -1,5 +1,7 @@
 package edu.spring.ex06;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -13,44 +15,82 @@ import edu.spring.ex06.domain.FeedVO;
 import edu.spring.ex06.persistence.FeedDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
 @WebAppConfiguration
 public class FeedDAOTest {
-	private static final Logger logger =
-			LoggerFactory.getLogger(FeedDAOTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(FeedDAOTest.class);
 
-	private static final String NAMESPACE =
-			"edu.spring.ex06.FeedMapper";
-	
+	private static final String NAMESPACE = "edu.spring.ex06.FeedMapper";
+
 	@Autowired
 	private FeedDAO dao;
-	
+
 	@Test
 	public void testDAO() {
-		testInsert();
+//		testInsert();
 //		testSelectAll();
+//		testSelect();
 //		testUpdate();
 //		testDelete();
+		testSelectAllbyId();
 	}// end testDAO()
-	
-	/*
-	 * private int feedId;
-	   private String feedContent;
-	   private String userId;
-	   private String userNickname;
-	   private String userProfile;
-	   private int replyCount;
-	   private int likeCount;
-	   private Date feedDate;
-	   private String musicTitle;
-	 * 
-	 * */
 
-//	--------------------------------------------------
+	// --------------------------------------------------
 	private void testInsert() {
 		FeedVO vo = new FeedVO(0, "피드내용", "유저아이디", "유저닉네임", "프로필사진", 0, 0, null, "음악제목");
-		int result = dao.insert(NAMESPACE, vo);
-		logger.info(result + "행 삽입");
+		int result = dao.insert(vo);
+		logger.info("♠ 결과 : " + result + "행 삽입");
 	}// end testInsert
 
+//	--------------------------------------------------
+
+	private void testSelectAll() {
+		List<FeedVO> list = dao.selectAll(null);
+		logger.info("총 개수 : " + list.size() + "개");
+		for (FeedVO vo : list) {
+			logger.info("♠ 결과 : " + vo.toString());
+		}
+	}// end testSelectAll
+
+//	--------------------------------------------------
+
+	private void testSelect() {
+		FeedVO vo = dao.select(null, 6);
+		logger.info("♠ 결과 : " + vo.toString());
+	}// end testSelect
+
+//	--------------------------------------------------
+
+	private void testUpdate() {
+		FeedVO vo = new FeedVO(6, "2차 내용", null, "2차 닉네임", "2차 프로필", 0, 0, null, "2차 음악제목");
+		int result = dao.update(null, 6, vo);
+
+		if (result == 1) {
+			logger.info("♠ 수정 성공");
+		}
+
+	}// end testUpdate
+
+//	--------------------------------------------------
+
+	private void testDelete() {
+		FeedVO vo = new FeedVO(6, null, null, null, null, 0, 0, null, null);
+		int result = dao.delete(null, 6);
+		
+		if(result == 1) {
+			logger.info("♠ 삭제 성공");
+		}
+
+	}// end testDelete
+
+//	--------------------------------------------------
+
+	private void testSelectAllbyId() {
+		List<FeedVO> list = dao.selectAllbyId("유저아이디");
+		logger.info("총 개수 : " + list.size() + "개");
+		for (FeedVO vo : list) {
+			logger.info("♠ 결과 : " + vo.toString());
+		}
+		
+	}// end testSelectAllbyId
 }
